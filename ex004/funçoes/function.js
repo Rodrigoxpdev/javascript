32eb0062121ca51e1ef5c7462963ca62f788cf26
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-08 16:25:04 -0300

    .

--- ex004/funçoes/function.js
+++ ex004/funçoes/function.js
@@ -43,25 +43,80 @@
 // const valor1 = Number(window.prompt("valor1"));
 // const valor2= Number(window.prompt("valor2"));
 
+
+//-----------------------------------------------------------------------------------
 // SOMA
 // const soma = valor1 + valor2
 // window.alert(`${valor1} + ${valor2} é: ${soma}`);
 
+
+//-----------------------------------------------------------------------------------
 // SUBTRAÇAO
 // const sub = valor1 - valor2
 // window.alert(`${valor1} - ${valor2} é: ${sub}`);
 
 
+//-----------------------------------------------------------------------------------
 // MULTIPLICAÇAO
 // const mult = valor1 * valor2
 // window.alert(`${valor1} * ${valor2} é: ${mult}`);
 
 
+//-----------------------------------------------------------------------------------
 // DIVISÃO
 // const div = valor1 / valor2 
 // window.alert(`${valor1} / ${valor2} = ${div}`)
 
+
+
+
 // ===================================================================================
+// NUMERO INTEIRO
+// Number.parseInt;
+// const n1 = Number.parseInt(window.prompt("digite o 1 numero"));
+
+// NUMERO REAL OU METADE
+// Number.parseFloat;
+// const n2 = Number.parseFloat(window.prompt("digite o 2 numero"));
+
+// NUMERO INTEIRO OU REAL PODE UTILIZAR SOMENTE O: "Number" O JS POR SI SÓ RECONHECE.
+// const n1 = Number(window.prompt("digite o 1 numero"));
+// const n2 = Number(window.prompt("digite o 2 numero"));
+
+
+
+// const n1 = Number(window.prompt("digite o 1 numero"))
+// const n2 = Number(window.prompt("digite o 2 numero"))
+
+// const nota = n1 + n2
+// window.alert(`sua nota é: ${nota} pontos` )
+
+// ========================================================================================
+// write = MOSTRA A MENSAGEM NA TELA
+// writeln = MOSTRA A MENSAGEM NA TELA QUEBRA PARA LINHA DE BAIXO
+
+//PEGA O DOCUMENTO E MOSTRA NA TELA DENTRO DO BODY
+// document.writeln(`Seu nome tem `);
+
+// EX:
+// const nome = window.prompt("qual e seu nome?")
+// document.write(`seu nome tem ${nome.length} letras <br/>` )
+// document.writeln(`nome em maiusculo ${nome.toUpperCase()} <br/>`);
+// document.writeln(`nome em minusculo ${nome.toLowerCase()}`)
+
+// ----------------------------------------------------------------------------
+
+
+
+// const nome = window.prompt("seu nome ?")
+// const idade = window.prompt("sua idade ?")
+// const profissao = window.prompt("sua profissão ?")
+// const experiencia = window.prompt("quanto tempo de experiencia ?")
+
+// window.alert(`seu nome é ${nome} idade ${idade} anos sua profissão e ${profissao} com experiencia de ${experiencia}`)
+
+
+
 
 
 

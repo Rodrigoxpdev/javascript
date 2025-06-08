e383050c5b6bb4d8d46da2c9e003ef08243c71e4
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-08 14:49:29 -0300

    .

--- ex004/funçoes/function.js
+++ ex004/funçoes/function.js
@@ -6,7 +6,6 @@
 //--------------------------------------------------------------
 
 
-
 //-----------------COMANDOS NO NAVEGADOR ------------------------
 // const nome = window.prompt('Qual é seu nome?')
 // const sobreNome = window.prompt('Sobrenome')
@@ -15,12 +14,66 @@
 // window.alert(`seu nome é  ${nome} ${sobreNome} idade ${idade} anos sua profissão é ${profissao}  !`)
 
 //---------------------------------------------------------------
+// const confirme = window.prompt("Deseja prosseguir com as perguntas?")
+// const nome2 = window.prompt("Qual é seu nome ?")
+// const sobreNome2 = window.prompt("Seu sobrenome")
+// const idade2 = window.prompt("Digite sua idade")
+// const profissao2 = window.prompt("Qual é a sua profissão ?")
+// window.alert(`Olá ${nome2} ${sobreNome2} sua idade é  ${idade2} anos sua profissão é ${profissao2}`)
 
-const nome2 = window.prompt("Qual é seu nome ?")
-const sobreNome2 = window.prompt("Seu sobrenome")
-const idade2 = window.prompt("Digite sua idade")
-const profissao2 = window.prompt("Qual é a sua profissão ?")
-window.alert(`Olá ${nome2} ${sobreNome2} sua idade é  ${idade2} anos sua profissão é ${profissao2}`)
+//----------------------------------------------------------------
+
+
+
+
+
+
+//=====================================================================================
+
+// DESSA FORMA ESTOU DIZENDO QUE A INFORMAÇAO QUE ESTA INDO DENTRO DO PROMPT E NUMERICA!
+// const n1 = Number(window.prompt("digite um numero"))
+// const n2 = Number(window.prompt("digite outro numero"))
+// const s = n1 + n2
+// window.alert(`A soma de ${n1} + ${n2} é igual a ${s} `);
+// const s = window.alert(`A soma de: ${} ${} `)
+
+//=====================================================================================
+
+// EXERCITANDO
+// const valor1 = Number(window.prompt("valor1"));
+// const valor2= Number(window.prompt("valor2"));
+
+// SOMA
+// const soma = valor1 + valor2
+// window.alert(`${valor1} + ${valor2} é: ${soma}`);
+
+// SUBTRAÇAO
+// const sub = valor1 - valor2
+// window.alert(`${valor1} - ${valor2} é: ${sub}`);
+
+
+// MULTIPLICAÇAO
+// const mult = valor1 * valor2
+// window.alert(`${valor1} * ${valor2} é: ${mult}`);
+
+
+// DIVISÃO
+// const div = valor1 / valor2 
+// window.alert(`${valor1} / ${valor2} = ${div}`)
+
+// ===================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

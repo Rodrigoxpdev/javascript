345dbcc3f95c1ac330c1b569d865ad4266638903
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-11 22:23:15 -0300

    .

--- ex007/funçoes.js
+++ ex007/funçoes.js
@@ -1,17 +1,17 @@
 // =======================================
+const p1 = window.document.getElementsByTagName('p')[1]
+
+//window.document.writeln('esta escrito assim: ' + p1.innerHTML)
 
 
+//const div = window.document.getElementById('msg')
+// div.style.background = 'red'
+// div.innerText = 'ESTOU AQUARDANDO...'
 
 
+// const d = window.document.getElementsByClassName('msg')[0]
+// d.innerText = 'ola'
 
-
-
-
-
-
-
-
-
-
-
+const d = window.document.querySelector('div.msg')
+d.style.background = 'red'
 

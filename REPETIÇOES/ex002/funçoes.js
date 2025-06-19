7ad466edc817636501dca31ee9cb0be20b3f3844
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-19 12:02:59 -0300

    ,

---
+++ REPETIÇOES/ex002/funçoes.js
@@ -0,0 +1,55 @@
+// let c = 1
+
+// while (c <= 10){
+//     console.log(`${c}`)
+//     c++
+// }
+
+console.log('vai começar..')
+for (let c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

49be9e1a0b0f0cce937fa1b447fb747ab29f228c
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-22 14:33:30 -0300

    estudando os aray

---
+++ VARIAVEIS-COMPOSTAS/EX001/funçoes.js
@@ -0,0 +1,129 @@
+const num = [5,8,4]
+
+//ADICIONANDO NUMEROS NA ULTIMA POSIÇAO AUTOMATICO USANDO 'PUSH'
+// num.push(8,7,9,90) 
+
+// COMO SABER O COMPRIMENTO USANDO length A POSIÇAO
+//num.length
+
+// USANDO SORT() PARA ORGANIZAR EM ORDEM CRESCENTE
+//num.sort()
+
+
+num.sort()
+num.push(100)
+console.log(num)
+console.log(`O VETOR TEM: ${num.length} POSIÇÕES`)
+console.log(`O PRIMEIRO VALOR É: ${num[0]}`)
+let pos = num.indexOf(4)
+console.log(`o valor 4 eta na posiçao ${pos}`)
+
+
+// for (let pos = 0; pos<num.length; pos++){
+//     console.log(pos)
+// }
+
+
+//BUSCANDO VALORES
+// num.indexOf(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ VARIAVEIS-COMPOSTAS/EX001/vetornatela.js
@@ -0,0 +1,46 @@
+let valores = [8, 1, 7, 4, 2, 9] //lista
+console.log(valores) //mostrando valores todos de uma so vez
+
+// for(let pos = 0; pos < valores.length; pos++){
+//     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+// }
+
+//FORMA SIMPLIFICADA
+for(let pos in valores ){
+        console.log(`A posição ${pos} tem o valor ${valores[pos]}`);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

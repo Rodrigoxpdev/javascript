f3e68f4dd682015a60be7739d30393fe35349959
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-22 13:18:37 -0300

    EXERCICIO DE TABUADA

--- CONDIÇOES/ex017/funçoes.js
+++ CONDIÇOES/ex017/funçoes.js
@@ -1,34 +1,27 @@
+function tabuada(){
+    // PEGANDO OS ID
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
 
-function carregar(){
-    const msg = document.getElementById('msg')
-    const img = document.getElementById('imagem')
-    const data = new Date() //DATA ATUAL
-    const hora = data.getHours() //HORA ATUAL
-    const min = data.getMinutes() //MINUTOS ATUAL
-    // const seg = data.getSeconds() //SEGUNDOS EM TEMPO REAL
-    // const hora = 9
-    const sd = document.getElementById('sd')
-    msg.innerHTML = `Agora são ${hora}:${min}`
-
-
-    if (hora >= 0 && hora < 12){
-        // MANHA 
-        img.src = '../ex005/imagens/foto-manha.png'
-        document.body.style.background = "#fddf64";
-        sd.innerHTML = "Bom dia..";
-    }else if (hora >= 12 && hora < 18){
-        // TARDE
-        document.body.style.background = "#f2a855";
-        img.src = '../ex005/imagens/foto-tarde.png'
-        sd.innerHTML = "Bom tarde..";
+    
+    
+    // CONDIÇAO
+    if (num.value.length == 0){//SE O NUMERO DIGITADO FOR IGUAL A 0 
+        window.alert('digite um numero')
     }else{
-        // NOITE
-        document.body.style.background = "#493752";
-        img.src = '../ex005/imagens/foto-noite.png'
-        sd.innerHTML = 'Boa noite..'
+        // CONVERTENDO PARA NUMERO
+        let n = Number(num.value) //CONVERTENDO PARA NUMERO VLR DIGITADO
+        let c = 1 //CONTADOR IGUAL A 1
+        tab.innerHTML = '' //LIMPANDO OS VALORES ANTES DE EXECUTRAR
+        while (c <= 10) { //EMQUANTO C FOR MEN0R OU IGUAL A 10
+            let item = document.createElement('option')//CRIANDO ELEMENTO
+            item.text = `${n} x ${c} = ${n*c}`//ITEM DO ELEMENTO
+            tab.appendChild(item) //ITEM FILHO
+            tab.value = `tab${c}`//VENDO O VALOR SELECIONADO
+            c++ //ADICIONANDO UM VALOR A MAIS EM LOOP 
+        }
     }
 }
-
 
 
 

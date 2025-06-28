de9d97937f22b7de3e397d71684b2c955f19bbbb
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-28 18:19:39 -0300

    analizador de numeros

--- CONDIÇOES/EX018/funçoes.js
+++ CONDIÇOES/EX018/funçoes.js
@@ -1,5 +1,5 @@
 let num = document.getElementById('fnum')
-let lista = document.getElementById('flista')
+let lista = document.getElementById('lista')
 let res = document.getElementById('res')
 let valores = []
 
@@ -13,29 +13,56 @@ function isNumero(n){
 
 //SE O NUMERO NAO FOR ENCONTRADO NA LISTA
 // -1 = NUMERO NAO ENCONTRADO NA LISTA!
-function inLista(n, l){
-    if(l.indexOF(Number(n)) != -1){
-        return true 
-    }else{
-        return false
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+    return true;
+    } else {
+    return false;
     }
 }
 
+//BOTAO DE ADICIONAR
 function adicionar(){
-    if(isNumero(num.value) && !inLista(num.value, valores)){
-        window.alert('tudo ok')
+    if (isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `valor ${num.value} adicionado`
+        flista.appendChild(item)
+        res.innerHTML = ''
     }else{
         window.alert('Valor invalido ou já adicionado na lista.')
     }
+    num.value = ''
+    num.focus()
 }
 
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('adicione um numero')
+    }else{
+        let tot = valores.length
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0
+        let media = 0
 
-
-
-
-
-
-
+        for (let pos in valores){   
+            soma += valores[pos]
+            if (valores[pos] > maior){
+                maior = valores[pos]
+                if(valores[pos] < menor)
+                menor =  valores[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo temos ${tot} numeros cadastrados.</p>`
+        res.innerHTML += `<p> o maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p> o menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p> somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p> a media total é ${media}.</p>`
+    }
+}
 
 
 

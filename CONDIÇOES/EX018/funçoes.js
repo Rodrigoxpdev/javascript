10ae2b00d1efbfca8b094eb2b19d86f0f3dc7fb2
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-28 18:30:46 -0300

    ADICIONANDO COMENTARIOS

--- CONDIÇOES/EX018/funçoes.js
+++ CONDIÇOES/EX018/funçoes.js
@@ -1,8 +1,11 @@
+//ARMAZENANDO DADOS USANDO O 'LET'
 let num = document.getElementById('fnum')
 let lista = document.getElementById('lista')
 let res = document.getElementById('res')
 let valores = []
 
+
+// FUNÇAO PARA SABER SE É NUMERO, E SE ESTA ENTRE 1 A 100
 function isNumero(n){
     if(Number(n) >= 1 && Number(n) <= 100){
         return true
@@ -11,8 +14,8 @@ function isNumero(n){
     }
 }
 
-//SE O NUMERO NAO FOR ENCONTRADO NA LISTA
-// -1 = NUMERO NAO ENCONTRADO NA LISTA!
+// VERIFICANDO SE O NUMERO JA ESTA NA LISTA
+//SE O NUMERO NAO FOR ENCONTRADO NA LISTA -1 = NUMERO NAO ENCONTRADO NA LISTA!
 function inLista(n, l) {
     if (l.indexOf(Number(n)) != -1) {
     return true;
@@ -36,6 +39,7 @@ function adicionar(){
     num.focus()
 }
 
+//BOTAO DE FINALIZAR
 function finalizar(){
     if (valores.length == 0){
         window.alert('adicione um numero')
@@ -45,7 +49,6 @@ function finalizar(){
         let menor = valores[0];
         let soma = 0
         let media = 0
-
         for (let pos in valores){   
             soma += valores[pos]
             if (valores[pos] > maior){

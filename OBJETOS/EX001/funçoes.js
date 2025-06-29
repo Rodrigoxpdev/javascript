4a7e7dd84b5bd1bf5e6bfd785b63bbbdd38d671c
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-06-29 00:36:44 -0300

    curso basico de javascript

---
+++ OBJETOS/EX001/funçoes.js
@@ -0,0 +1,71 @@
+// OBJETOS EM JS
+
+let amigo = {
+    nome: 'rodrigo', 
+    sexo:'M', 
+    peso: '68.1',
+    engordar(p=0){
+        console.log(`Engordou`)
+        this.peso += p
+    }}
+    
+    amigo.engordar(2)
+    console.log(`${amigo.nome} pesa ${amigo.peso} kg`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
